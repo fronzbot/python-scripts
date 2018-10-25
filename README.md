5ce7e772a52d7e54fac7ee194167231055d2ec5c
Kevin Fronczak <kfronczak@gmail.com>
2018-10-25 11:41:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# python-scripts
+
+This repo contains a bunch of python scripts I run on periodically on my system.  I use this in conjunction with my [python docker](https://github.com/fronzbot/docker-python) which allows me to schedule scripts via a `config.yaml` file.
+
+# Current scripts:
+
+- `make_gif.py`: Creates a gif from a series of images (I use this for [home-assistant](https://home-assistant.io))
---
+++ make_gif.py
@@ -0,0 +1,77 @@
+"""Makes a gif from a series of images."""
+import os
+import time
+import shutil
+import imageio
+
+# Edit these variables
+IMAGE_DIR = '/share/hass/raw_images'
+BACKUP_DIR = '/share/hass/backup'
+SAVE_DIR = '/share/hass/gifs'
+
+# In seconds
+TARGET_GIF_LENGTH = 5
+
+# Max number of images to use in a GIF
+MAX_IMAGES = 48
+
+
+def get_sorted_files():
+    """Return sorted filelist."""
+    files = os.listdir(IMAGE_DIR)
+    fullfiles = [os.path.join(IMAGE_DIR, name) for name in files]
+    sortedfiles = sorted(fullfiles, key=os.path.getmtime)
+
+    return sortedfiles
+
+
+def backup_images():
+    """Backup images when we exceed limit."""
+    # First backup gifs
+    gif = os.path.join(SAVE_DIR, 'output.gif')
+    if os.path.isfile(gif):
+        timestamp = time.strftime("%Y_%m_%d_%H%M%S")
+        shutil.move(gif, os.path.join(SAVE_DIR, 'old'))
+        oldfile = "{}/{}/output.gif".format(SAVE_DIR, 'old')
+        newfile = "{}/{}/{}.gif".format(SAVE_DIR, 'old', timestamp)
+        os.rename(oldfile, newfile)
+
+    # Now, sort files based on date
+    sortedfiles = get_sorted_files() 
+
+    print("Found {} images".format(len(sortedfiles)))
+    # Now check if we even need to bother backing up    
+    delete_range = len(sortedfiles) - MAX_IMAGES
+    if delete_range > 0:
+        for index in range(0, delete_range):
+            print("Backing up {}".format(sortedfiles[index]))
+            shutil.move(sortedfiles[index], BACKUP_DIR)
+
+    return
+
+
+def create_new_gif():
+    """Use imageio to create a gif."""
+    output = os.path.join(SAVE_DIR, 'temp.gif')
+    framerate = int(MAX_IMAGES / TARGET_GIF_LENGTH)
+
+    images = []
+    files = get_sorted_files()
+
+    with imageio.get_writer(output, mode='I', fps=framerate) as writer:
+        for filename in files:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    # And now optimize the gif
+    final = os.path.join(SAVE_DIR, 'output.gif')
+    command = "convert {} -fuzz 15% -layers Optimize {}".format(output, final)
+    os.system(command)
+    os.remove(output)
+
+    return
+
+
+if __name__ == '__main__':
+    backup_images()
+    create_new_gif()
