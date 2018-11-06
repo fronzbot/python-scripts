94f6ff1fb59cd6a866a90a0dfbf6eafd3bfd9d9a
Kevin Fronczak <kfronczak@gmail.com>
2018-11-06 13:07:44 -0500

    Fixed broken logger

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ LOGGER = logging.getLogger('pycron')
 LOGGER.setLevel(logging.DEBUG)
 
 HANDLER = logging.handlers.SysLogHandler('/dev/log')
-FORMATTER = logging.Formatter('%(module)s.%(funcName)s: %(levelName)s %(message)s')
+FORMATTER = logging.Formatter('%(levelname)s [%(module)s.%(funcName)s]: %(message)s')
 HANDLER.setFormatter(FORMATTER)
 
 LOGGER.addHandler(HANDLER)
